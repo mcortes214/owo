32e98ba0e75d2173dc76bca57537dee47167431a
Maxi Cortes <mcortes.na@gmail.com>
2019-04-06 19:18:07 -0300

    Estilos mejorados

--- generador.js
+++ generador.js
@@ -1,10 +1,14 @@
+var arrayUs = ['u','ū','ú','ǔ','ù','ŭ','û','ü','ů','ų','ũ','ű','ȕ','ṳ','ṵ','ṷ','ṹ','ṻ','ǖ','ǘ','ǚ','ǜ','ủ','ụ','ư','ừ','ử','ữ','ứ','ự'];
+//inicio
 $('document').ready(function(){
-  var arrayUs = ['u','ū','ú','ǔ','ù','ŭ','û','ü','ů','ų','ũ','ű','ȕ','ṳ','ṵ','ṷ','ṹ','ṻ','ǖ','ǘ','ǚ','ǜ','ủ','ụ','ư','ừ','ử','ữ','ứ','ự'];
-  console.log(nuevoUwu());
+  $('.uwuContent').html(nuevoUwu());
 });
+//evento de click
 $('.btnGenerar').click(function(){
   $('.uwuContent').html(nuevoUwu());
 });
+
+//declaración de función
 function nuevoUwu(){
   var a, b, r, s;
   //1er componente
