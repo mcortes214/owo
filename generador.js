0c79542484edc68f2daa829d48a8ef4c40646504
Maxi Cortes <mcortes.na@gmail.com>
2019-04-06 19:05:22 -0300

    Funcion implementada

--- generador.js
+++ generador.js
@@ -1,5 +1,19 @@
 $('document').ready(function(){
-  var arrayUs[] = {'u','ū','ú','ǔ','ù','ŭ','û','ü','ů','ų','ũ','ű','ȕ','ṳ','ṵ','ṷ','ṹ','ṻ','ǖ','ǘ','ǚ','ǜ','ủ','ụ','ư','ừ','ử','ữ','ứ','ự'};
-  console.log('Hola mundo');
-  console.log(arrayUs);
+  var arrayUs = ['u','ū','ú','ǔ','ù','ŭ','û','ü','ů','ų','ũ','ű','ȕ','ṳ','ṵ','ṷ','ṹ','ṻ','ǖ','ǘ','ǚ','ǜ','ủ','ụ','ư','ừ','ử','ữ','ứ','ự'];
+  console.log(nuevoUwu());
 });
+$('.btnGenerar').click(function(){
+  $('.uwuContent').html(nuevoUwu());
+});
+function nuevoUwu(){
+  var a, b, r, s;
+  //1er componente
+  r = parseInt(Math.random()*arrayUs.length);
+  a = arrayUs[r];
+  //2do componente
+  r = parseInt(Math.random()*arrayUs.length);
+  b = arrayUs[r];
+  //concatenar componentes
+  s = a + 'w' + b;
+  return s;
+}
